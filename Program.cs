d4c921c78b4f1f3827abf9afaf94dc846270d5d9
Hannes Davik <Hannes.davik@gmail.com>
2024-10-17 10:28:06 +0200

    guid

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ändring i feature");
+            Console.WriteLine("Ändring i feature" + Guid.NewGuid);
             Console.WriteLine("Hej Git");
         }
     }
